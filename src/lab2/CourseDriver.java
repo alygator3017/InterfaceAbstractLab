bbcc45d4166844b1068c6878996ffe0331402485
Aly <aschmidt36@my.wctc.edu>
2015-09-15 20:00:35 -0500

    added comments into driver

--- src/lab2/CourseDriver.java
+++ src/lab2/CourseDriver.java
@@ -6,7 +6,28 @@
 package lab2;
 
 /**
- *
+ * For me, this was easier to set up than the abstract class, even though 
+ * they were essentially the same. Smoother is the way I'd describe it
+ * I wasn't worried about what properties may go where and if I needed to implement 
+ * any concrete methods. 
+ * I think it also helped that I had all the driver code pre-made and had to 
+ * tailor it to the specific lab.
+ * 
+ * Good- I liked the face that everything was easy to make, while the implementation
+ *      probably takes longer, in the long run everything is essentially smoother
+ *      and more put together. The developer of the subclasses still has the option
+ *      of how they want to implement (like the way they want to deal with JOptionPane)
+ *      but overall, the fact that I didn't have to deal with properties or implementation
+ *      is nice.
+ * Bad- I don't have much to say when it comes to bad. I mean it's a horse a piece.
+ *      While you lose the option to add properties to the interface or concrete methods,
+ *      you retain the ability for the developer of the subclasses to have options.
+ *      Not to mention that they know what they are getting (all the stuff we've
+ *      covered already on why abstract classes are dangerous- bad code, don't know what you have,
+ *      may be misusing the methods, plus you might override something you aren't supposed to, 
+ *      you don't have the problem with interfaces). Makes this architecture easier to use
+ *      and manage. 
+ * 
  * @author Alyson
  */
 public class CourseDriver {
